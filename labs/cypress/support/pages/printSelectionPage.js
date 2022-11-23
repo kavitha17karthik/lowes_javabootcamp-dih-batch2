fab37bf9af3fd6c703371d548d01d6d53d8fbec3
kavitha17karthik <105655455+kavitha17karthik@users.noreply.github.com>
2022-11-23 18:35:33 +0530

    Delete labs/cypress directory

--- labs/cypress/support/pages/printSelectionPage.js
+++
@@ -1,7 +0,0 @@
-class PrintSelectionPage {
-  validateInstructionalMsg(msg) {
-    cy.get('[data-qtag-receipt-instructionalmsg="true"]').should('have.text', msg);
-  }
-}
-
-export default new PrintSelectionPage();
